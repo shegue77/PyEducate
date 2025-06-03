d95a6a9157c6defd99087ee4cb1df4ecfccb52a9
shegue77 <makinduteo@gmail.com>
2025-06-03 19:28:59 +0100

    Initial Commit

--- start.py
+++ start.py
@@ -1,5 +1,13 @@
 from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QLineEdit, QVBoxLayout, QPushButton
+from sys import argv as sys_argv, exit as sys_exit
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys_argv)
+    window = MainWindow()
+    window.show()
+    sys_exit(app.exec())
