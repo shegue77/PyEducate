ae0f2cdef4619af26e814c2cd5b7c9c9e5ae27c0
shegue77 <makinduteo@gmail.com>
2025-06-03 19:23:50 +0100

    Initial Commit

---
+++ start.py
@@ -0,0 +1,5 @@
+from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QLineEdit, QVBoxLayout, QPushButton
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
