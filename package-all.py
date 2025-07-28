1606faf9ecd1ea338a88579b2e044e2ae6bae6da
shegue77 <197374785+shegue77@users.noreply.github.com>
2025-07-28 22:23:48 +0100

    Allowed cross-compatibility

--- package-all.py
+++ package-all.py
@@ -5,9 +5,9 @@ from shutil import copy2, copytree
 stuff = ("LICENSE", "SOURCE")
 dist_parts = ("client", "server", "lesson-editor")
 
-lesson_edit_path = join('server', 'lesson-editor.py')
-server_path = join('server', 'server.py')
-client_path = join('client', 'client.py')
+lesson_edit_path = join("server", "lesson-editor.py")
+server_path = join("server", "server.py")
+client_path = join("client", "client.py")
 print(system(f"pyinstaller --onedir --windowed {lesson_edit_path}"))
 print(system(f"pyinstaller --onefile {server_path}"))
 print(system(f"pyinstaller --onedir --windowed {client_path}"))
@@ -24,6 +24,6 @@ for part in dist_parts:
 
     destination_path = join("dist", str(part), "THIRD-PARTY-LICENSES")
     try:
-        copytree('THIRD-PARTY-LICENSES', destination_path)
+        copytree("THIRD-PARTY-LICENSES", destination_path)
     except Exception:
         pass
